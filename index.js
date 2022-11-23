fa21c36dd2004553e41e81a5c20f069ab05d5f72
Iddahodhiambo <iddahawuor@yahoo.com>
2022-11-23 10:18:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let Distance;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(Distance){
+  if (Distance <= 400) {
+   return "This one is on me!"
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }else if (Distance > 2000 && Distance < 2500) {
+    return "I will gladly take your thirty bucks."
+
+  }else if (Distance > 2500) {
+    return "No can do."
+
+  }
 }
+let city;
+function ternaryCheckCity(city) {
+  if(city === 'NYC') {
+    return "Ok, sounds good."
+  } else if(city !== 'NYC') {
+    return "No go."
+  }
+  }
+  let tip;
+  function switchOnCharmFromTip(tip)  {
+    if(tip === "generous") {
+      return "Thank you so much."
+    }
+  else if (tip === "not as generous") {
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
+}
+  
+
+
+
+
+
